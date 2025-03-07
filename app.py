453134872f2b50e460ab6607b1937c6e7fa11a6f
ND Long <ngduylong442k@gmail.com>
2025-03-07 17:17:13 +0700

    update render deploy

--- app.py
+++ app.py
@@ -31,5 +31,6 @@ def index():
     return render_template("index.html", img_data=img_data)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ biến môi trường, mặc định 5000 nếu không có
+    port = int(os.environ.get("PORT", 8080))
+    print(f"Running on port: {port}")
     app.run(host="0.0.0.0", port=port)
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-echo "PORT=$PORT"
+echo "Starting app on PORT=$PORT"
 gunicorn -w 4 -b 0.0.0.0:$PORT app:app
